0a6f6e0fd31be775c1e4d24a8832347de4bfae79
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-10-10 23:43:51 -0700

    changed my mind lol

--- quicksort.hpp
+++ quicksort.hpp
@@ -2,6 +2,8 @@
 #ifndef MERGE_SORT_H
 #define MERGE_SORT_H
 using itr = std::vector<int>::iterator;
+int partition(int*,int,int);
 void quickSort(int*,int,int);
+itr partition(std::vector<int>&,itr,itr);
 void quickSort(std::vector<int>&,itr,itr);
 #endif
