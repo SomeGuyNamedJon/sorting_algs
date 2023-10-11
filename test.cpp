a7f3e02e38e4520f3dbb761ed9fa577d8130814e
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-10-10 22:43:15 -0700

    testing arr quicksort

--- test.cpp
+++ test.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
-#include "mergesort.hpp"
+#include <stdlib.h>
 #include "quicksort.hpp"
 using namespace std;
 
 int main(){
-  int a[] = {1, 5, 7, 10, 19};
-  int b[] = {2, 4, 6, 11};
-  size_t size_a = sizeof(a)/sizeof(*a);
-  size_t size_b = sizeof(b)/sizeof(*b);
-  
-  int* result = merge(a, b, size_a, size_b);
-
-  for(int i=0; i<size_a+size_b; i++){
-    cout << result[i] << " "; 
+  size_t size = 10;
+  int* arr = new int[size];
+  for(int i=0; i<size; i++){
+    arr[i] = rand() % 500;
+    cout << arr[i] << " ";
   }
+  cout << endl << endl;
+
+  quickSort(arr,0,size-1);
+
+  for(int i=0; i<size; i++)
+    cout << arr[i] << " ";
   cout << endl;
 }
