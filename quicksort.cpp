9bdce5efa17783f9b3bec18219140f1a731b3bb8
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-10-10 22:00:14 -0700

    changed ints to iterator for vector implementation

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+using itr = vector::iterator;
 
 int partition(int* arr, int low, int high){}
 void quicksort(int* arr, int low, int high){}
 
-int partition(vector<int>& vect, int low, int high){}
-void partition(vector<int>& vect, int low, int high){}
+int partition(vector<int>& vect, itr low, itr high){}
+void partition(vector<int>& vect, itr low, itr high){}
--- quicksort.hpp
+++ quicksort.hpp
@@ -1,8 +1,9 @@
 #include <vector>
 #ifndef MERGE_SORT_H
 #define MERGE_SORT_H
+using itr = std::vector::iterator;
 int partition(int*,int,int);
 void quickSort(int*,int,int);
-int partition(std::vector<int>&,int,int);
-void quickSort(std::vector<int>&,int,int);
+int partition(std::vector<int>&,itr,itr);
+void quickSort(std::vector<int>&,itr,itr);
 #endif
