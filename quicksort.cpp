b4dfc928b7a81e78ad8659ea36b5fb48161084f8
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-10-10 22:19:08 -0700

    implemented arr partition

--- quicksort.cpp
+++ quicksort.cpp
@@ -2,7 +2,32 @@
 using namespace std;
 using itr = vector::iterator;
 
-int partition(int* arr, int low, int high){}
+int partition(int* arr, int low, int high){
+  int pivot = arr[low];
+  int count = 0;
+
+  for(int i=low+1; i<=high; i++){
+    if(arr[i] <= pivot)
+      count++;
+  }
+
+  int pivotIndex = low + count;
+
+  swap(arr[pivotIndex], arr[low]);
+
+  int i=low, j=high;
+  while(i < pivotIndex && j > pivotIndex){
+    while(arr[i] <= pivot)
+      i++;
+    while(arr[j] > pivot)
+      j--;
+
+    if(i < pivotIndex && j > pivotIndex)
+      swap(arr[i++], arr[j--]);
+  }
+
+  return pivotIndex;
+}
 void quickSort(int* arr, int low, int high){
   if(low < high){
     int pIndex = partition(arr, low, high);
