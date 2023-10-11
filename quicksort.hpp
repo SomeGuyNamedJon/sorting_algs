1c6723f7f3ad45d03975fc2bbfce689b4b0e3476
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-10-10 23:11:34 -0700

    updated quicksort header file

--- quicksort.hpp
+++ quicksort.hpp
@@ -2,8 +2,6 @@
 #ifndef MERGE_SORT_H
 #define MERGE_SORT_H
 using itr = std::vector<int>::iterator;
-int partition(int*,int,int);
 void quickSort(int*,int,int);
-itr partition(std::vector<int>&,itr,itr);
 void quickSort(std::vector<int>&,itr,itr);
 #endif
