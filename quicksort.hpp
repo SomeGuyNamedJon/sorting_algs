90c3acb0200812ea0376e934ec0fae2444e7398f
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-10-10 23:04:37 -0700

    updated quicksort header

--- quicksort.hpp
+++ quicksort.hpp
@@ -4,6 +4,6 @@
 using itr = std::vector<int>::iterator;
 int partition(int*,int,int);
 void quickSort(int*,int,int);
-int partition(std::vector<int>&,itr,itr);
+itr partition(std::vector<int>&,itr,itr);
 void quickSort(std::vector<int>&,itr,itr);
 #endif
