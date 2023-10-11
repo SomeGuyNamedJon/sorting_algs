ac7b4b155ec700c75c39c55bff31694a44584f7b
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-10-10 22:04:34 -0700

    fixed some things + quicksort implementation

--- quicksort.cpp
+++ quicksort.cpp
@@ -3,7 +3,14 @@ using namespace std;
 using itr = vector::iterator;
 
 int partition(int* arr, int low, int high){}
-void quicksort(int* arr, int low, int high){}
+void quickSort(int* arr, int low, int high){
+  if(low < high){
+    int pIndex = partition(arr, low, high);
+
+    quickSort(arr, low, pIndex-1);
+    quickSort(arr, pIndex+1, high);
+  }
+}
 
 int partition(vector<int>& vect, itr low, itr high){}
-void partition(vector<int>& vect, itr low, itr high){}
+void quickSort(vector<int>& vect, itr low, itr high){}
